898681be83e7949cb47d762c103f3c791998c122
azozulyuk <kvitochka101010@gmail.com>
2026-04-06 12:19:19 -0400

    Update Drink.cpp

--- Drink.cpp
+++ Drink.cpp
@@ -15,7 +15,7 @@ Version Date      Reason
 V0.7    2025/04/1  Completing code
 V0.8    2025/04/4  Debugged
 V0.9    -         Asked prof for help for XYZ
-V1.0    2026/04/4  Ready for submission
+V1.0    2026/04/6  Ready for submission
 -----------------------------------------------------------
 I have done all the coding by myself and only copied the code
 that my professor provided to complete my work for function main,ect.
@@ -41,7 +41,7 @@ namespace seneca {
     ostream& Drink::print(ostream& os) const {
         os << m_name;
 
-        int dots = 33 - strlen(m_name);
+        int dots = 28 - strlen(m_name);
         if (dots < 0) dots = 0;
 
         for (int i = 0; i < dots; i++) os << '.';
@@ -59,7 +59,7 @@ namespace seneca {
         cout << "         Drink Size Selection" << endl;
         cout << "          1- Small" << endl;
         cout << "          2- Medium" << endl;
-        cout << "          3- Large" << endl;
+        cout << "          3- Larg" << endl;
         cout << "          4- Extra Large" << endl;
         cout << "          0- Back" << endl;
         cout << "         > ";
